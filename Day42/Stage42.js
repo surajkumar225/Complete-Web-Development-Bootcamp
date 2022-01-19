b5da9f625bb912b2158cdf80c202d31f7f6c0076
Suraj Kumar <surajk2542@gmail.com>
2022-01-19 20:40:53 +0530

    Day42 of WebDev Learning

---
+++ Day42/Stage42.js
@@ -0,0 +1,31 @@
+console.log("Hey");
+let x = 3;
+let y = 6;
+let z;
+z0 = x+y;
+z1 = x-y;
+z2 = x*y;
+z3 = x/y;
+
+// Math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.4);
+z = Math.ceil(5.3);
+z2 = Math.floor(-5.3);
+z = Math.abs(5);
+z0 = Math.sqrt(64);
+z = Math.pow(2, 3);
+z = Math.min(2, 3, 34,234, 2342,34);
+z3 = Math.max(2, 3, 34,234,34);
+z = Math.random();
+z = 100*Math.random()
+z1 = Math.ceil(50 + (200-100)*Math.random())
+// a = (0, 1)
+// a100 = a*100 = (0, 100)
+// a10_100 = 10+a*(100 - 10)
+console.log(z0);
+console.log(z1);
+console.log(z2);
+console.log(z3);
