bd5bb932f98381258312e8324487e55d5cfc3630
Suraj Kumar <surajk2542@gmail.com>
2022-03-03 22:29:31 +0530

    JavaScript Stuffs

---
+++ Day83/Stage83.js
@@ -0,0 +1,47 @@
+console.log("This is project 4");
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+let validEmail = false;
+let validPhone = false;
+let validUser = false;
+$('#failure').hide();
+$('#success').hide();
+
+// console.log(name, email, phone);
+name.addEventListener('blur', ()=>{
+    console.log("name is blurred");
+    // Validate name here
+    let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
+    let str = name.value;
+    console.log(regex, str);
+    if(regex.test(str)){
+        console.log('Your name is valid');
+        name.classList.remove('is-invalid');
+        validUser = true;
+    }
+    else{
+        console.log('Your name is not valid');
+        name.classList.add('is-invalid');
+        validUser = false;
+        
+    }
+})
+
+email.addEventListener('blur', ()=>{
+    console.log("email is blurred");
+    // Validate email here
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    let str = email.value;
+    console.log(regex, str);
+    if(regex.test(str)){
+        console.log('Your email is valid');
+        email.classList.remove('is-invalid');
+        validEmail = true;
+    }
+    else{
+        console.log('Your email is not valid');
+        email.classList.add('is-invalid');
+        validEmail = false;
+    }
+})
