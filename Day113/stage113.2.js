ee4fe695a258662de07de6366c905f1a31eda5a6
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-02 21:57:31 +0530

    Nodejs

--- Day113/stage113.2.js
+++ Day113/stage113.2.js
@@ -8,6 +8,6 @@ const server = http.createServer((req, res) => {
     res.end(fileContent)
 })
 
-server.listen(80, '127.0.0.1', ()=> {
-    console.log("Listening on port 80")
+server.listen(8000, '127.0.0.1', ()=> {
+    console.log("Listening on port 8000")
 })
