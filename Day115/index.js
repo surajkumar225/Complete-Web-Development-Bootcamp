a66396fa700bcf276e20cf78dc9b3b6c03e51673
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-03 21:57:19 +0530

    Nodejs

---
+++ Day115/index.js
@@ -0,0 +1,11 @@
+// const mod = require("./mod");
+// console.log(mod)
+
+// const average = require("./mod");
+// console.log(average([5,4]))
+const mod = require("./mod");
+console.log(mod.name)
+// console.log(mod.avg([5,4]))
+
+
+console.log("This is index");
