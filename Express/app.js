0d4d79d5c65095cb3af0d129705922060e1573c4
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-05 22:28:12 +0530

    Pug

--- Express/app.js
+++ Express/app.js
@@ -3,36 +3,51 @@ const path = require("path")
 const app = express();
 const port = 80;
 
-// For serving static files
-app.use('/static', express.static('static'))
+// Express specific stuff
+app.use('/static', express.static('static')) // For serving static files
 
-//set the template engine as pug
-app.set('view engine', 'pug')
+//Pug specific stuff
+app.set('view engine', 'pug') //set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) //Set the views directory
 
-//Set the views directory
-app.set('views', path.join(__dirname, 'views'))
 
-//Pug demo endpoint
-app.get("/demo", (req, res)=> {
-    res.status(200).render('demo', { title: 'Hey Trinity', message: 'This is amazing!' })
-});
+// Endpoints
+app.get('/', (req, res) => {
+    const con = "This is something u have never used before"
+    const params = {'title': "Playing wid Pug", 'content': con}
+    res.status(200).render('index.pug', params);
+})
 
-app.get("/", (req, res)=> {
-    res.status(200).send("This is home page of my first express app")
-});
-
-app.get("/about", (req, res)=> {
-    res.send("This is about page of my first express app")
-});
-
-app.post("/about", (req, res)=> {
-    res.send("This is a post request about page of my first express app")
-});
-
-app.get("/this", (req, res)=> {
-    res.status(404).send("This page is not found")
-});
-
+// Start the server
 app.listen(port, ()=> {
     console.log(`The application started successfully on port ${port}`)
 })
+
+
+
+
+
+
+
+
+
+//Pug demo endpoint
+// app.get("/demo", (req, res)=> {
+//     res.status(200).render('demo', { title: 'Hey Trinity', message: 'This is amazing!' })
+// });
+
+// app.get("/", (req, res)=> {
+    //     res.status(200).send("This is home page of my first express app")
+// });
+
+// app.get("/about", (req, res)=> {
+//     res.send("This is about page of my first express app")
+// });
+
+// app.post("/about", (req, res)=> {
+//     res.send("This is a post request about page of my first express app")
+// });
+
+// app.get("/this", (req, res)=> {
+//     res.status(404).send("This page is not found")
+// });
