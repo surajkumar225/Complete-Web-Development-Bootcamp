ef7588818c13b2d01686bfe98c6fe043422e91b4
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-06 21:35:58 +0530

    Express/Pug

--- Express/app.js
+++ Express/app.js
@@ -1,20 +1,35 @@
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
 const app = express();
 const port = 80;
 
 // Express specific stuff
 app.use('/static', express.static('static')) // For serving static files
+app.use(express. urlencoded())
 
 //Pug specific stuff
 app.set('view engine', 'pug') //set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) //Set the views directory
 
-
 // Endpoints
 app.get('/', (req, res) => {
     const con = "This is something u have never used before"
     const params = {'title': "Playing wid Pug", 'content': con}
+    res.status(200).render('index.pug', params);
+})
+
+app.post('/', (req, res) => {
+    name = req.body.name
+    email = req.body.email
+    age = req.body.age
+    key = req.body.key
+    more = req.body.more
+
+    let outputToWrite = `The name of the client is ${name}, ${age} years old, ${email}, ${key}. More about him/her: ${more}`
+
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': "Your form has been submitted successfully"}
     res.status(200).render('index.pug', params);
 })
 
